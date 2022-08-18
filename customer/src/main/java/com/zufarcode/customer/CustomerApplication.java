03e775e533bd0a2cae80f244909115298efd6872
zufar-sunagatov <zufar.sunagatov@gmail.com>
2022-08-19 00:40:34 +0100

    zipkin and sleuth were added

--- customer/src/main/java/com/zufarcode/customer/CustomerApplication.java
+++ customer/src/main/java/com/zufarcode/customer/CustomerApplication.java
@@ -1,17 +1,25 @@
 package com.zufarcode.customer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.zufarscode.customer",
+                "com.zufarscode.amqp",
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.zufarscode.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
     }
